6703b5b1a383712ba5012988088fe7eaa5060cda
Finlay Maguire <finlaymaguire@gmail.com>
2021-01-22 13:36:04 -0400

    Add incomplete date filter to extraction script

--- extract_seqs.py
+++ extract_seqs.py
@@ -17,11 +17,18 @@ def check_file(path: str) -> Path:
     else:
         raise argparse.ArgumentTypeError(f"{path} can't be read")
 
-def filter_metadata(metadata_fp, query, include_reference):
+def filter_metadata(metadata_fp, query, include_reference, exclude_incomplete_dates):
     """
     Parse and filter metadata
     """
     df = pd.read_csv(str(metadata_fp), sep='\t')
+
+    # remove XX in dates
+    df['date'] = df['date'].str.replace('-XX', '')
+
+    if exclude_incomplete_dates:
+        df = df[df['date'].str.len() == 10]
+
     filt_df = df.query(query)
     if include_reference:
         ref_df = df[df['strain'].isin(['Wuhan/Hu-1/2019', 'Wuhan/WH01/2019'])]
@@ -70,15 +77,19 @@ if __name__ == "__main__":
                          help="Pandas query e.g., \"pangolin_lineage=='B.1.1.28'\"")
     parser.add_argument("-o", "--output_prefix", default="filtered",
                         help="Prefix for output filtered metadata and seqs")
-    parser.add_argument("-r", "--include_reference", default=False,
+    parser.add_argument("--include_reference", default=False,
                         action='store_true',
                         help="Include Wuhan/Hu-1/2019 & Wuhan/WH01/2019")
+    parser.add_argument("--exclude_incomplete_dates", default=False,
+                        action='store_true',
+                        help="Remove any isolates without a complete date")
 
 
     args = parser.parse_args()
 
     filtered_metadata = filter_metadata(args.nextmeta, args.query,
-                                        args.include_reference)
+                                        args.include_reference,
+                                        args.exclude_incomplete_dates)
 
     filtered_seqs = filter_seqs(args.nextfasta, filtered_metadata)
 
